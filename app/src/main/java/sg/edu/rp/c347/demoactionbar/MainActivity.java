92cd0c15d052a458b654069b6e67f8f24c4bd4ee
18004885 <18004885@myrp.edu.sg>
2020-07-24 10:34:47 +0800

    Final

--- app/src/main/java/sg/edu/rp/c347/demoactionbar/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/demoactionbar/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.options, menu);
         return super.onCreateOptionsMenu(menu);
+
+        
     }
 
 }
